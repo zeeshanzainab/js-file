b8bf9789af42c9be7ea311d387642ba39556f48c
zainabzaeeshan <zeeshanzainab724@gmail.com>
2026-01-17 16:04:21 +0500

    js file

---
+++ script.js
@@ -0,0 +1,24 @@
+document.write("<h1>hello </h1>")
+document.write("<br>")
+var studentName="ayesha"
+document.write(studentName)
+document.write("<br>")
+var age=24
+document.write(age)
+document.write(typeof(age))
+console.log("javascript is prograaming language")
+document.write("<br>")
+var isActive = true
+document.write("<br>")
+document.write(typeof(isActive))
+document.write("<br>")
+document.write(typeof(studentName))
+document.write("<br>")
+var isActive = true
+var instituteName="Aptech"
+document.write(instituteName)
+document.write("<br>")
+
+document.write(typeof(instituteName))
+document.write(90+80
+)
